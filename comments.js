1367d27b9c1e018c153af50d59d8654d94a1f165
Ghostibles <73785892+Ghostibles@users.noreply.github.com>
2023-07-31 07:46:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,39 +1,94 @@
 //create web server
-var http = require('http');
-//create file system
-var fs = require('fs');
-//create url
-var url = require('url');
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const Comment = require('./models/comment');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/comments');
+const db = mongoose.connection;
 
-//create server
-http.createServer(function (request, response) {
-    //get the path name
-    var pathname = url.parse(request.url).pathname;
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
 
-    //print the pathname
-    console.log("Request for " + pathname + " received.");
+db.on('error', function(err){
+  console.log(err);
+});
+//set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+//set body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+//set routes
+app.get('/', function(req, res){
+  Comment.find({}, function(err, comments){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.render('index', {
+        title: 'Comments',
+        comments: comments
+      });
+    }
+  });
+});
 
-    //read the file from the server
-    fs.readFile(pathname.substr(1), function (err, data) {
-        if (err) {
-            //print error
-            console.log(err);
-            //HTTP status: 404 : NOT FOUND
-            //Content Type: text/plain
-            response.writeHead(404, {'Content-Type': 'text/html'});
-        } else {
-            //Page found
-            //HTTP status: 200 : OK
-            //Content Type: text/plain
-            response.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/comments/add', function(req, res){
+  res.render('add_comment', {
+    title: 'Add comment'
+  });
+});
 
-            //write the content of the file to response body
-            response.write(data.toString());
-        }
-        //send the response body
-        response.end();
-    });
-}).listen(8081);
+app.post('/comments/add', function(req, res){
+  let comment = new Comment();
+  comment.author = req.body.author;
+  comment.text = req.body.text;
+  comment.save(function(err){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/');
+    }
+  });
+});
 
-//print message
-console.log('Server running at http://
+app.get('/comments/edit/:id', function(req, res){
+  Comment.findById(req.params.id, function(err, comment){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.render('edit_comment', {
+        title: 'Edit comment',
+        comment: comment
+      });
+    }
+  });
+});
+
+app.post('/comments/edit/:id', function(req, res){
+  let comment = {};
+  comment.author = req.body.author;
+  comment.text = req.body.text;
+  let query = {_id: req.params.id};
+  Comment.update(query, comment, function(err){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.delete('/comments/:id', function(req, res){
+  let query = {_id: req.params.id};
+  Comment.remove(query, function(err){
+    if (err) {
+      console.log(err);
